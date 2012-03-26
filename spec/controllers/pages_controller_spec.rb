7e27ddbac31129c7f83093689c4c5c16b768532b
Shekar Pasumarthi <shekarp@pcweb.com>
2012-03-26 16:33:19 -0700

    stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_tile = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
     it "should have right tile" do
       get 'home'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+                      :content => @base_tile + " | Home")
     end
   end
 
@@ -23,7 +27,7 @@ describe PagesController do
     it "should have right tile" do
       get 'contact'
       response.should have_selector("title",
-                                     :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_tile + " | Contact")
     end
   end
 
@@ -35,7 +39,7 @@ describe PagesController do
     it "should have right tile" do
       get 'about'
       response.should have_selector("title",
-                                     :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_tile + " | About")
     end
   end
 end
